1926f0ba7e6faf80584b67707ad5ebe5e68c3be1
vineeth <vineethclm@gmail.com>
2016-08-02 12:54:24 +0530

    First commit

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Test Git</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+</head>
+
+<body>
+<div style="margin:50px auto; text-align:center;">
+	<h3>Hello World!</h3>
+	<p>My First Git Program</p>
+</div>
+</body>
+</html>
